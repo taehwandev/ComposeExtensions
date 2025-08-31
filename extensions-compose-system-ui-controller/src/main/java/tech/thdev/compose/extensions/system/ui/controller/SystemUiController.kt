97d06f7f4b8cae245bd6d2059f7cdb7767dc6b45
easyhooon <mraz3068@gmail.com>
2025-09-01 01:18:33 +0900

    fix: system-ui-controller package name
    exteions -> extensions

--- extensions-compose-system-ui-controller/src/main/java/tech/thdev/compose/extensions/system/ui/controller/SystemUiController.kt
+++ extensions-compose-system-ui-controller/src/main/java/tech/thdev/compose/extensions/system/ui/controller/SystemUiController.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package tech.thdev.compose.exteions.system.ui.controller
+package tech.thdev.compose.extensions.system.ui.controller
 
 import android.app.Activity
 import android.content.Context
